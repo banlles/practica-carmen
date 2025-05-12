610cb82c3ca29bfea376477f4c547bd8e05de576
Banlles <janbanles@gmail.com>
2025-05-12 12:47:04 +0200

    aa

--- src/main/java/org/grupo04/Main.java
+++ src/main/java/org/grupo04/Main.java
@@ -278,9 +278,11 @@ public class Main {
 				String hotelOriginal = entrada.next();
 				System.out.println("Introduce el nuevo número de habitaciones:");
 				int habitacionesEdit = entrada.nextInt();
+				System.out.println("Introduce el nuevo nombre del hotel:");
+				String nuevoNombre = entrada.next();
 				System.out.println("Introduce la nueva ubicación del hotel:");
 				String nuevaUbicacion = entrada.next();
-				Hotel hotelEditar = new Hotel(habitacionesEdit, hotelOriginal, nuevaUbicacion);
+				Hotel hotelEditar = new Hotel(habitacionesEdit, nuevoNombre, nuevaUbicacion);
 				hotelEditar.merge();
 				break;
 			case 3:
